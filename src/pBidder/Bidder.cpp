0b6d9375999cfd3c61d0250e88289c83cc3a5f62
Mike Bogochow <mbogochow@hotmail.com>
2015-12-16 18:55:07 -0500

    Removed som comments

--- src/pBidder/Bidder.cpp
+++ src/pBidder/Bidder.cpp
@@ -25,15 +25,6 @@
 
 Bidder::Bidder(void)
 {
-//  g = new Graph(__edges, __num_edges, __weights, __num_nodes);
-//
-//  // TODO add me to the graph
-//
-//  allocated.reserve(__num_nodes);
-//  unallocated.reserve(__num_nodes);
-//
-//  for (Vertex i = 0; i < __num_nodes; i++)
-//    unallocated.push_back(i);
   g = nullptr;
 
   rtc = 0;
@@ -50,42 +41,6 @@ Bidder::~Bidder(void)
   if (g != nullptr)
     delete g;
 }
-
-//#define _a 0
-//#define _b 1
-//#define _c 2
-//#define _d 3
-//#define _e 4
-//#define _f 5
-//#define _g 6
-//#define _h 7
-//#define _i 8
-//
-//const size_t __num_nodes = 9;
-//const size_t __num_edges = 36;
-//Edge __edges[__num_edges] = {
-//  Edge(_a, _b), Edge(_a, _c), Edge(_a, _d), Edge(_a, _e), Edge(_a, _f), Edge(_a, _g), Edge(_a, _h), Edge(_a, _i),
-//  Edge(_b, _c), Edge(_b, _d), Edge(_b, _e), Edge(_b, _f), Edge(_b, _g), Edge(_b, _h), Edge(_b, _i),
-//  Edge(_c, _d), Edge(_c, _e), Edge(_c, _f), Edge(_c, _g), Edge(_c, _h), Edge(_c, _i),
-//  Edge(_d, _e), Edge(_d, _f), Edge(_d, _g), Edge(_d, _h), Edge(_d, _i),
-//  Edge(_e, _f), Edge(_e, _g), Edge(_e, _h), Edge(_e, _i),
-//  Edge(_f, _g), Edge(_f, _h), Edge(_f, _i),
-//  Edge(_g, _h), Edge(_g, _i),
-//  Edge(_h, _i)
-//};
-//int __weights[__num_edges] = {
-//    4,       8,       11,      8,      7,        4,       2,      9,
-//    14,      10,      2,       1,      6,        7,       4,
-//    8,       11,      8,       7,      4,        2,
-//    9,       14,      10,      2,      1,
-//    6,       7,       4,       8,
-//    11,      8,       7,
-//    4,       2,
-//    9 };
-//Point __Pointations[__num_nodes] = {
-//    Point(7, 0), Point(15, 15), Point(30, 15), Point(45, 15),
-//    Point(60, 0), Point(45, -15), Point(30, -15), Point(15, -15), Point(22, 0)
-//};
 
 bool
 Bidder::OnNewMail(MOOSMSG_LIST &NewMail)
