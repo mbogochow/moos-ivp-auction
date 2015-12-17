02d133770b5874522219f96c99de8ff96f63956f
Mike Bogochow <mbogochow@hotmail.com>
2015-12-16 19:03:12 -0500

    Removed sleep before quit for bidder to test whether it is necessary with the new exit method

--- src/pBidder/Bidder.cpp
+++ src/pBidder/Bidder.cpp
@@ -145,7 +145,7 @@ Bidder::Iterate(void)
       // Exit pBidder
       doNotify("EXITED_NORMALLY", "pBidder");
       // Ensure all data makes it to the MOOSDB
-      std::this_thread::sleep_for(std::chrono::seconds(10));
+//      std::this_thread::sleep_for(std::chrono::seconds(10));
       // OR
         // SetQuitOnFailedIterate(true);
         // ret = false;
